6b34b2777ed6fe6e708fa9657d4117507c238b2d
Biswajeet Sahoo <sahoobiswajeet29@gmail.com>
2026-03-24 11:57:03 +0530

    Connect MongoDB using async/await

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const bodyParser = require("body-parser");
-// require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
 
@@ -12,18 +12,35 @@ app.use(express.json());
 // app.use(express.static("public"));
 
 // DB connection
-// mongoose.connect(process.env.MONGO_URI)
-// .then(() => console.log("MongoDB Connected"))
-// .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 // Routes
 app.get("/", (req, res) => {
   res.send("FinPulse is running 🚀");
 });
 
+// TRANSACTION
+//Create
+app.post("/transactions", (req, res) => {
+  const data = req.body;
+
+  console.log(data);
+  res.json({
+    message: "Transaction created",
+    data: data,
+  });
+});
+
+//Get
+app.get("/transactions", (req, res) => {
+  res.send("hello");
+});
+
 // const transactionRoutes = require("./routes/transactionRoutes");
 
 // app.use("/transactions", transactionRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
